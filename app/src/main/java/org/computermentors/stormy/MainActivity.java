8bda8a2afc743c9bd4261d7117c7eb94939cd6f2
N. Dukes <nyjel.dukes@gmail.com>
2019-11-26 09:26:17 -0500

    API/URL framework placed

--- app/src/main/java/org/computermentors/stormy/MainActivity.java
+++ app/src/main/java/org/computermentors/stormy/MainActivity.java
@@ -3,12 +3,43 @@ package org.computermentors.stormy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "8125d754a73a5db6ebbc808d0b6f307c";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forcastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forcastURL)
+                .build();
+
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful())
+                Log.v(TAG, response.body().string());
+        } catch (IOException e) {
+            Log.e(TAG, "IO exception caught", e);
+        }
     }
 }
