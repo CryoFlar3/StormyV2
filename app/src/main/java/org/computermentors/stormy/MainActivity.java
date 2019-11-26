ee67e5582b9abff8c456eb26e091901a961706a7
Nyjel <nyjel.dukes@gmail.com>
2019-11-26 12:17:18 -0500

    App has access to the internet

--- app/src/main/java/org/computermentors/stormy/MainActivity.java
+++ app/src/main/java/org/computermentors/stormy/MainActivity.java
@@ -1,13 +1,15 @@
 package org.computermentors.stormy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 
+import androidx.appcompat.app.AppCompatActivity;
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -33,13 +35,23 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
 
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful())
-                Log.v(TAG, response.body().string());
-        } catch (IOException e) {
-            Log.e(TAG, "IO exception caught", e);
-        }
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                try {
+                    if (response.isSuccessful())
+                        Log.v(TAG, response.body().string());
+                } catch (IOException e) {
+                    Log.e(TAG, "IO exception caught", e);
+                }
+            }
+        });
+
+        Log.d(TAG, "Main UI code is running!");
     }
 }
